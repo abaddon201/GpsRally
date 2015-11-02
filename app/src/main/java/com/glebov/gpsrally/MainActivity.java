d369c5f28b33d83c786811a3227720e5d0a2a2b1
aglebov2 <aglebov2@gmail.com>
2015-11-02 23:19:44 +0300

    fixes long tap on listview

--- app/src/main/java/com/glebov/gpsrally/MainActivity.java
+++ app/src/main/java/com/glebov/gpsrally/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -67,10 +68,11 @@ public class MainActivity extends AppCompatActivity {
         listView.addHeaderView(v);
         // Assign adapter to ListView
         listView.setAdapter(dataAdapter);
-        listView.setOnLongClickListener(new View.OnLongClickListener() {
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public boolean onLongClick(View v) {
-                Toast.makeText(MainActivity.this, "long tap",Toast.LENGTH_SHORT).show();
+            public boolean onItemLongClick(AdapterView<?> arg0, View v,
+                                           int index, long arg3) {
+                Toast.makeText(MainActivity.this, "long tap", Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
