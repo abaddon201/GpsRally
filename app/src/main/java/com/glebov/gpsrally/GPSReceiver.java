49a0c3833189e0d3699faff70cdfca89c78606bc
Aleksandr Glebov <glebov@altell.ru>
2015-11-03 21:06:47 +0300

    add gps ui elements

--- app/src/main/java/com/glebov/gpsrally/GPSReceiver.java
+++ app/src/main/java/com/glebov/gpsrally/GPSReceiver.java
@@ -13,9 +13,20 @@ import android.os.Bundle;
  * Created by glebov on 03.11.15.
  */
 public class GPSReceiver implements GpsStatus.Listener, LocationListener {
+    public interface GPSUI {
+        void onFixChanged(boolean fixed);
+
+        void onDeviceStatusChanged(int state);
+
+        void onSatelitesChanged(int all, int good);
+
+        void onLocation(Location location);
+    }
+
     private final Context mContext;
     private LocationManager locationManager;
 
+    private GPSUI ui;
     private boolean hasFix = false;
 
     public GPSReceiver(Context context) {
@@ -23,10 +34,15 @@ public class GPSReceiver implements GpsStatus.Listener, LocationListener {
     }
 
     @SuppressWarnings("ResourceType")// defined in manifest
-    public void start() {
+    public void start(GPSUI _ui) throws IllegalArgumentException {
+        if (_ui == null)
+            throw new IllegalArgumentException("_ui can't be null");
+
+        ui = _ui;
         hasFix = false;
         locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates("gps", 0, 0, this);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+//        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
         locationManager.addGpsStatusListener(this);
     }
 
@@ -36,6 +52,7 @@ public class GPSReceiver implements GpsStatus.Listener, LocationListener {
             locationManager.removeUpdates(this);
             locationManager.removeGpsStatusListener(this);
         }
+        ui = null;
     }
 
     @Override
@@ -46,33 +63,54 @@ public class GPSReceiver implements GpsStatus.Listener, LocationListener {
         switch (event) {
             case GpsStatus.GPS_EVENT_FIRST_FIX:
                 hasFix = true;
+                if (ui != null)
+                    ui.onFixChanged(true);
                 break;
             case GpsStatus.GPS_EVENT_SATELLITE_STATUS:
                 GpsStatus gpsStatus = locationManager.getGpsStatus(null);
                 int all = 0;
+                int good = 0;
                 Iterable<GpsSatellite> satellites = gpsStatus.getSatellites();
                 for (GpsSatellite ignored : satellites) {
                     all++;
+                    if (ignored.usedInFix())
+                        good++;
                 }
+                if (ui != null)
+                    ui.onSatelitesChanged(all, good);
                 break;
         }
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        hasFix = true;
+        if (!hasFix) {
+            hasFix = true;
+            if (ui != null)
+                ui.onFixChanged(true);
+        }
+        if (ui != null)
+            ui.onLocation(location);
     }
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
+        if (ui != null)
+            ui.onDeviceStatusChanged(status);
         switch (status) {
             case LocationProvider.AVAILABLE:
+                if (!hasFix && (ui != null))
+                    ui.onFixChanged(true);
                 hasFix = true;
                 break;
             case LocationProvider.OUT_OF_SERVICE:
+                if (hasFix && (ui != null))
+                    ui.onFixChanged(false);
                 hasFix = false;
                 break;
             case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                if (hasFix && (ui != null))
+                    ui.onFixChanged(false);
                 hasFix = false;
                 break;
         }
@@ -86,6 +124,8 @@ public class GPSReceiver implements GpsStatus.Listener, LocationListener {
     @Override
     public void onProviderDisabled(String provider) {
         hasFix = false;
+        if (ui != null)
+            ui.onFixChanged(false);
     }
 
     @SuppressWarnings("ResourceType")
--- app/src/main/java/com/glebov/gpsrally/MainActivity.java
+++ app/src/main/java/com/glebov/gpsrally/MainActivity.java
@@ -2,6 +2,9 @@ package com.glebov.gpsrally;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationProvider;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
@@ -10,17 +13,27 @@ import android.text.InputFilter;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText planedDistance;
-    EditText planedAvgSpeed;
-    EditText planedTime;
+    private EditText planedDistance;
+    private EditText planedAvgSpeed;
+    private EditText planedTime;
 
-    DatabaseHelper dbHelper;
+    private ImageView gpsFixedIco;
+    private ImageView gpsAvailableIco;
+    private TextView gpsSatelites;
+    private TextView gpsAccuracity;
+
+    private DatabaseHelper dbHelper;
     private SimpleCursorAdapter dataAdapter;
+
+    private GPSReceiver gpsReceiver;
+    private GPSReceiver.GPSUI gpsCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,11 @@ public class MainActivity extends AppCompatActivity {
         planedAvgSpeed = (EditText) findViewById(R.id.planedAvgSpeed);
         planedDistance.setFilters(new InputFilter[]{new DecimalDigitsInputFilter(3, 2)});
         planedAvgSpeed.setFilters(new InputFilter[]{new DecimalDigitsInputFilter(3, 2)});
+
+        gpsFixedIco = (ImageView) findViewById(R.id.gpsFix);
+        gpsAvailableIco = (ImageView) findViewById(R.id.gpsAvailable);
+        gpsSatelites = (TextView) findViewById(R.id.gpsSatelites);
+        gpsAccuracity = (TextView) findViewById(R.id.gpsAccuracity);
 
         dbHelper = new DatabaseHelper(this);
         dbHelper.open();
@@ -81,6 +99,44 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-    }
+        gpsCallback = new GPSReceiver.GPSUI() {
+            @Override
+            public void onFixChanged(boolean fixed) {
+                if (fixed)
+                    gpsFixedIco.setBackgroundColor(Color.GREEN);
+                else
+                    gpsFixedIco.setBackgroundColor(Color.RED);
+            }
 
+            @Override
+            public void onDeviceStatusChanged(int status) {
+                switch (status) {
+                    case LocationProvider.AVAILABLE:
+                        gpsAvailableIco.setBackgroundColor(Color.GREEN);
+                        break;
+                    case LocationProvider.OUT_OF_SERVICE:
+                        gpsAvailableIco.setBackgroundColor(Color.RED);
+                        break;
+                    case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                        gpsAvailableIco.setBackgroundColor(Color.YELLOW);
+                        break;
+                }
+
+            }
+
+            @Override
+            public void onSatelitesChanged(int all, int good) {
+                String t = String.format("%2d", good) + "/" + String.format("%2d", all);
+                gpsSatelites.setText(t);
+            }
+
+            @Override
+            public void onLocation(Location location) {
+                String t = String.format("%.2f", location.getAccuracy());
+                gpsAccuracity.setText(t);
+            }
+        };
+        gpsReceiver = new GPSReceiver(this);
+        gpsReceiver.start(gpsCallback);
+    }
 }
